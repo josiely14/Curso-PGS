5b224280af7339e63d745fa1366060de70d108c4
josiely14 <josiely.lopes03@icloud.com>
2026-09-25 08:36:21 -0300

    Add files via upload

---
+++ alunos.py
@@ -0,0 +1,21 @@
+'''
+Criação: Crie um dicionário chamado alunos contendo
+3 alunos como chaves e seus respectivos dicionários 
+internos com nome, idade e curso.
+'''
+
+aluno1 = {
+ "nome" : "Josy"
+    "idade" : 22,
+    "curso" : "programador"
+}
+aluno2 = {
+    "nome" : "Nara"
+    "idade" : 30,
+    "curso" : "programador"
+}
+aluno3 = {
+    "nome" : "Sofia"
+    "idade" : 19,
+    "curso" : "programador"
+}
